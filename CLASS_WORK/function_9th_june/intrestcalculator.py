287553d7f4ac30280e815ae852377d9bd81005ce
Preet Bansal <gujjarpreet2102@gmail.com>
2026-06-09 14:38:28 +0530

    first commit

---
+++ CLASS_WORK/function_9th_june/intrestcalculator.py
@@ -0,0 +1,9 @@
+#
+def simple_intrest(principal , rate ,time):
+    intrest = (principal * rate * time) / 100
+    return intrest 
+#----------------------------------------------
+
+principal = float(input("enter the principle amount :"))
+#validate the principle amount 
+
